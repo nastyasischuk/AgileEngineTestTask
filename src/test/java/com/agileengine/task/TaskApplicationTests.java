c069515118c9d11035b5fe0fe442d93540c1c2c7
Nastya <nastyasischuk@gmail.com>
2020-08-19 14:34:54 +0300

    init

---
+++ src/test/java/com/agileengine/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agileengine.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
